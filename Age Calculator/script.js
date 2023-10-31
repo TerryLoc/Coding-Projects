683f57529a16de5b09e55668c5c03d1c86130287
TerryLoc <terryloughran@gmail.com>
2023-10-31 12:21:56 +0000

    function calculation

--- Age Calculator/script.js
+++ Age Calculator/script.js
@@ -12,11 +12,34 @@ function calculateAge() {
 
   let today = new Date();
 
-  let d2 = birthDate.getDate();
-  let m2 = birthDate.getMonth() + 1;
-  let y2 = birthDate.getFullYear();
+  let d2 = today.getDate();
+  let m2 = today.getMonth() + 1;
+  let y2 = today.getFullYear();
 
-  return today - birthDate;
+  let d3, m3, y3;
+
+  y3 = y2 - y1;
+
+  if (m2 >= m1) {
+    m3 = m2 - m1;
+  } else {
+    y3--;
+    m3 = 12 + m2 - m1;
+  }
+
+  if (d2 >= d1) {
+    d3 = d2 - d1;
+  } else {
+    m3--;
+    d3 = getDaysInMonth(y1, m1) + d2 - d1;
+  }
+  if (m3 < 0) {
+    m3 = 11;
+    y3--;
+  }
+  console.log(y3, m3, d3);
 }
 
-calculateAge();
+function getDaysInMonth(year, month) {
+  return new Date(year, month, 0).getDate();
+}
