5debf0e60d3954cab4c702527f219a533e31b545
TerryLoc <terryloughran@gmail.com>
2023-11-01 13:15:03 +0000

    api update

--- Quote generator/script.js
+++ Quote generator/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const quote = document.getElementById('quote');
+const author = document.getElementById('author');
+const api_url = ' https://api.quotable.io/random';
+
+async function getQuote(url) {
+  const responce = await fetch(url);
+  let data = await responce.json();
+  quote.innerText = data.content;
+  author.innerText = data.author;
+}
+
+getQuote(api_url);
