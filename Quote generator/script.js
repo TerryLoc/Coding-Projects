c550549262f977454b9f61c2b6791c5d4d9dbd25
TerryLoc <terryloughran@gmail.com>
2023-11-01 16:54:44 +0000

    finished code

--- Quote generator/script.js
+++ Quote generator/script.js
@@ -3,6 +3,9 @@
 const quote = document.getElementById('quote');
 const author = document.getElementById('author');
 const api_url = ' https://api.quotable.io/random';
+const x_api = 'https://twitter.com/intent/tweet';
+
+const newQuote = document.getElementById('new-quote');
 
 async function getQuote(url) {
   const responce = await fetch(url);
@@ -11,4 +14,13 @@ async function getQuote(url) {
   author.innerText = data.author;
 }
 
-getQuote(api_url);
+function share() {
+  window.open(
+    'https://twitter.com/intent/tweet?text=' +
+      quote.innerText +
+      ' ---- A quote by ' +
+      author.innerText,
+    'Tweet Window',
+    'width = 600, height = 300'
+  );
+}
