73ba0897dc203a1264a48e39f9ca53d54f5d2212
TerryLoc <terryloughran@gmail.com>
2023-11-01 10:20:17 +0000

    innerHTML result

--- Age Calculator/script.js
+++ Age Calculator/script.js
@@ -2,6 +2,7 @@
 
 let userInput = document.getElementById('date');
 userInput.max = new Date().toISOString().split('T')[0];
+let result = document.getElementById('result');
 
 function calculateAge() {
   let birthDate = new Date(userInput.value);
@@ -37,7 +38,7 @@ function calculateAge() {
     m3 = 11;
     y3--;
   }
-  console.log(y3, m3, d3);
+  result.innerHTML = `You are ${y3} years, ${m3} months & ${d3} days young!`;
 }
 
 function getDaysInMonth(year, month) {
