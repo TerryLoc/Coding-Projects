c0aab953ffa6efead2f9ed47108e3ae3961ee653
TerryLoc <terryloughran@gmail.com>
2023-11-03 15:54:04 +0000

    Qr code running

--- QR code generator/script.js
+++ QR code generator/script.js
@@ -1,7 +1,14 @@
 'use strict';
 
-let imgBox = document.getElementById('imgbox');
-let qrImg = document.getElementById('qrImg');
+let imgBox = document.getElementById('imgBox');
+let qrImage = document.getElementById('qrImage');
 let qrText = document.getElementById('qrText');
 
-function generateQR() {}
+function generateQR() {
+  if (qrText.value.length > 0) {
+    qrImage.src =
+      'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=' +
+      qrText.value;
+    imgBox.classList.add('show-img');
+  }
+}
