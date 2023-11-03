b9b714bf57ad98e83cc7d5e159616268dde23fbc
TerryLoc <terryloughran@gmail.com>
2023-11-03 14:28:10 +0000

    function

--- QR code generator/script.js
+++ QR code generator/script.js
@@ -1,1 +1,7 @@
 'use strict';
+
+let imgBox = document.getElementById('imgbox');
+let qrImg = document.getElementById('qrImg');
+let qrText = document.getElementById('qrText');
+
+function generateQR() {}
