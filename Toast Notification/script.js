5c2b6f27cc6f931039256599a5e506c618242db8
TerryLoc <terryloughran@gmail.com>
2023-11-03 19:16:32 +0000

    start

--- Toast Notification/script.js
+++ Toast Notification/script.js
@@ -1,1 +1,9 @@
 'use strict';
+
+let toastBox = document.getElementById('toa');
+
+function showToast() {
+  let toast = document.createElement('div');
+  toast.classList.add('toast');
+  toast.innerText = 'Success';
+}
