1b09cebdc26cffabf3f2f15f431acddf56adf2b2
TerryLoc <terryloughran@gmail.com>
2023-11-06 11:02:45 +0000

    Play button function

--- Music Player/script.js
+++ Music Player/script.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const progress = document.getElementById('progress');
+const song = document.getElementById('song');
+const ctrlIcon = document.getElementById('ctrlIcon');
+
+song.onloadedmetadata = function () {
+  progress.max = song.duration;
+  progress.value = song.currentTime;
+};
+
+function playPause() {
+  if (ctrlIcon.classList.contains('fa-pause')) {
+    song.pause();
+    ctrlIcon.classList.remove('fa-pause');
+    ctrlIcon.classList.add('fa-play');
+  } else {
+    song.play();
+    ctrlIcon.classList.add('fa-pause');
+    ctrlIcon.classList.remove('fa-play');
+  }
+}
