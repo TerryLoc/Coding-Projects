8e4c74b50bb4e73e886dc0e5f614b9216f0a153d
TerryLoc <terryloughran@gmail.com>
2023-11-10 14:01:11 +0000

    finished pop up

--- A pop up/script.js
+++ A pop up/script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const popup = document.getElementById('popup');
+
+function openPop() {
+  popup.classList.add('openPopUp');
+}
+function closePop() {
+  popup.classList.remove('openPopUp');
+}
