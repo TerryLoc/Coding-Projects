f52d10dac2154a3c06245eb3abc81d168695fa74
TerryLoc <terryloughran@gmail.com>
2023-11-12 17:44:54 +0000

    passwprd toggle finished

--- Show password/script.js
+++ Show password/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const eyeIcon = document.getElementById('eye-icon');
+const password = document.getElementById('password');
+
+eyeIcon.onclick = function () {
+  if (password.type === 'password') {
+    password.type = 'text';
+    eyeIcon.src = 'eye-icons/eye-open.png';
+  } else {
+    password.type = 'password';
+    eyeIcon.src = 'eye-icons/eye-close.png';
+  }
+};
