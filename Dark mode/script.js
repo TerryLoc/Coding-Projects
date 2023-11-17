d4f641673862507b90fc5ab3c407eb0306b11899
TerryLoc <terryloughran@gmail.com>
2023-11-17 18:43:20 +0000

    finished dark mode

--- Dark mode/script.js
+++ Dark mode/script.js
@@ -1,1 +1,10 @@
 'use srict';
+
+let icon = document.getElementById('icon');
+
+icon.onclick = function () {
+  document.body.classList.toggle('dark-theme');
+  if (document.body.classList.contains('dark-theme')) {
+    icon.src = '/Dark mode/dark theme icon/sun.png';
+  } else icon.src = '/Dark mode/dark theme icon/moon.png';
+};
