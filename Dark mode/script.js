bc43af6263a3f5b1a2b0982c898bdcf2d9ca5a99
TerryLoc <terryloughran@gmail.com>
2023-11-20 10:28:22 +0000

    update

--- Dark mode/script.js
+++ Dark mode/script.js
@@ -1,10 +1,10 @@
-'use srict';
+"use srict";
 
-let icon = document.getElementById('icon');
+let icon = document.getElementById("icon");
 
 icon.onclick = function () {
-  document.body.classList.toggle('dark-theme');
-  if (document.body.classList.contains('dark-theme')) {
-    icon.src = '/Dark mode/dark theme icon/sun.png';
-  } else icon.src = '/Dark mode/dark theme icon/moon.png';
+  document.body.classList.toggle("dark-theme");
+  if (document.body.classList.contains("dark-theme")) {
+    icon.src = "/Dark mode/dark theme icon/sun.png";
+  } else icon.src = "/Dark mode/dark theme icon/moon.png";
 };
