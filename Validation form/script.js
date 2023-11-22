6e39196bac3d10f6235ab1c2c61904b76d29bcc6
TerryLoc <terryloughran@gmail.com>
2023-11-22 11:19:18 +0000

    extra JS

--- Validation form/script.js
+++ Validation form/script.js
@@ -20,3 +20,37 @@ function validateName() {
   nameError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
   return true;
 }
+
+function validatePhone() {
+  let number = document.getElementById('contact-phone').value;
+
+  if (number.length === 0) {
+    phoneError.innerText = 'Phone number is required';
+    return false;
+  }
+  if (number.length !== 10) {
+    phoneError.innerText = 'Number should be 10 digits';
+    return false;
+  }
+
+  if (!number.match(/^[0-9]{10}$/)) {
+    phoneError.innerText = 'Only digits please';
+    return false;
+  }
+  phoneError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+  return true;
+}
+
+function validateEmail() {
+  let email = document.getElementById('contact-email').value;
+
+  if (email.length === 0) {
+    emailError.innerText = 'Email is required';
+    return false;
+  }
+
+  if (!emailError.match(/))
+
+  emailError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+  return true;
+}
