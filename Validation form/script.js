627f2c6842c95e3ceebc948e0acefe2371c12579
TerryLoc <terryloughran@gmail.com>
2023-11-22 10:26:40 +0000

    JS file

--- Validation form/script.js
+++ Validation form/script.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const nameError = document.getElementById('name-error');
+const phoneError = document.getElementById('phone-error');
+const emailError = document.getElementById('email-error');
+const msgError = document.getElementById('msg-error');
+const submitError = document.getElementById('submit-error');
+
+function validateName() {
+  let name = document.getElementById('contact-name').value;
+
+  if (name.length === 0) {
+    nameError.innerText = 'Name is required';
+    return false;
+  }
+  if (!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)) {
+    nameError.innerText = 'Write full name';
+    return false;
+  }
+  nameError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+  return true;
+}
