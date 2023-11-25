9d1eced9e7dca2981c23542a1ef6839243a132df
TerryLoc <terryloughran@gmail.com>
2023-11-25 16:22:26 +0000

    scroll control

--- Image gallery/script.js
+++ Image gallery/script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const scrollControl = document.querySelector('.gallery');
+
+const backBtn = document.getElementById('backBtn');
+const nextBtn = document.getElementById('nextBtn');
+
+scrollControl.addEventListener('wheel', (evt) => {
+  evt.preventDefault();
+  scrollControl.scrollLeft += evt.deltaY;
+});
